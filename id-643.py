52921c2d4cdac6e32c4b5981dcfcdf8df7506ed6
madaodog <mohamed.azzarouali@student.kuleuven.be>
2024-07-23 12:17:30 +0200

    23/07

---
+++ id-643.py
@@ -0,0 +1,17 @@
+'''
+643. Maximum Average Subarray I
+Source: https://leetcode.com/problems/maximum-average-subarray-i/
+'''
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        left = 0
+        current_sum = 0
+        for i in range(k):
+            current_sum += nums[i]
+        maximum = current_sum
+        
+        for j in range(k, len(nums)):
+            current_sum = current_sum - nums[left] + nums[j]
+            maximum = max(maximum, current_sum)
+            left += 1
+        return maximum / k
